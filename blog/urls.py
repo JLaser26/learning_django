19500c7309000a49f79e789c13eaacfebd11323e
Jai <jaikamath169@gmail.com>
2026-02-09 21:23:21 +0530

    added edit and delete features

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home),
     path('post/<int:post_id>/', views.post_detail),
     path('create/', views.create_post),
+    path('edit/<int:post_id>/', views.edit_post),
+    path('delete/<int:post_id>/', views.delete_post),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -20,3 +20,21 @@ def create_post(request):
         form = PostForm()
 
     return render(request, 'create_post.html', {'form': form})
+
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'create_post.html', {'form': form})
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('/')
